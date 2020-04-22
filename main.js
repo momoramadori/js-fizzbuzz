582fedef038743bf6818b5d58e6a28840da3b717
Momo Ramadori <momoramadori8@gmail.com>
2020-04-22 21:13:29 +0200

    Stilato

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 //Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-
-//Segnarmi una array
-var numeri = [];
-//Inserire i numeri da 1 a 100 nel mio array
-for (var i = 0; i < 100; i++) {
-    numeri[i] = i + 1;
+//Creare una "lista" di 100 numeri
+for (var i = 1; i <= 100; i++) {
     //Devo riconoscere i multipli di 3
-    var modulo3 = numeri[i] % 3;
+    var modulo3 = i % 3;
     //Devo riconoscere i multipli di 5
-    var modulo5 = numeri[i] % 5;
-    //
+    var modulo5 = i % 5;
+    //Identifico una variabile anche per multipli di 15
+    var modulo15 = i % 15;
     //Devo sostituire i numeri con le varie Fizz,Buzz e FizzBuzz
-    if (modulo3 == 0 && modulo5!= 0) {
-        //Sostituisco con Fizz i multipli di 3 e non di 5
-        numeri[i] = "Fizz";
-    } else if (modulo3!= 0 && modulo5 == 0) {
-        //Sostituisco con Buzz i multipli non di 3 ma di 5
-        numeri[i] = "Buzz";
-    } else if (modulo3 == 0 && modulo5 == 0) {
-        //Sostituisco con FizzBuzz i multipli sia di 3 che di 5
-        numeri[i] = "FizzBuzz";
+    if ( modulo15 == 0 ) {
+        console.log('FizzBuzz');
+        document.getElementById("container").insertAdjacentHTML("beforeend",'<li class="fizzbuzz">FizzBuzz</li>');
+    } else if ( modulo5 == 0) {
+        console.log('Buzz')
+        document.getElementById("container").insertAdjacentHTML("beforeend", '<li class="buzz">Buzz</li>');
+    } else if ( modulo3 == 0) {
+        console.log('Fizz')
+        document.getElementById("container").insertAdjacentHTML("beforeend",'<li class="fizz">Fizz</li>' );
+    } else {
+        console.log(i);
+        document.getElementById("container").insertAdjacentHTML("beforeend", '<li>' + i + '</li>');
     }
-    console.log(numeri[i]);
 }
